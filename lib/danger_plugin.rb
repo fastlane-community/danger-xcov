3f256cc55143a60d753094aa41251364ad57906f
Carlos Vidal <nakioparkour@gmail.com>
2017-07-15 14:10:34 +0100

    taken new files added into account

--- lib/danger_plugin.rb
+++ lib/danger_plugin.rb
@@ -77,7 +77,8 @@ module Danger
 
     # Filters the files that haven't been modified in the current PR
     def process_report(report)
-      file_names =  @dangerfile.git.modified_files.map { |file| File.basename(file) }
+      file_names = @dangerfile.git.modified_files.map { |file| File.basename(file) }
+      file_names += @dangerfile.git.added_files.map { |file| File.basename(file) }
       report.targets.each do |target|
         target.files = target.files.select { |file| file_names.include?(file.name) }
       end
